0470c9ed3b1b8e3d72984d5dd82ca216aabb4574
Hackbright Student <info@hackbrightacademy.com>
2016-08-02 12:24:05 -0700

    Enabled signing up and logging in

--- server.py
+++ server.py
@@ -14,6 +14,7 @@ app = Flask(__name__)
 
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS']=False
 
 # Normally, if you use an undefined variable in Jinja2, it fails
 # silently. This is horrible. Fix this so that, instead, it raises an
@@ -35,12 +36,55 @@ def user_list():
     return render_template("user_list.html", users=users)
 
 @app.route("/register")
-# display register form (default method is GET)
+def display_registration_form():
+    """ Show sign up form """
+
+    return render_template("register_form.html")
+
 
 @app.route("/register", methods=['POST'])
-# process reg form
+def process_registration():
+    """ Add new user to database, sign them in, then redirect to homepage """
 
-when user hits submit, route to /register with method as post
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    user = User.query.filter_by(email=email).first()
+
+    if user is None:
+        new_user = User(email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        session['current_user'] = email
+        flash("Logged in as %s" % email)
+
+    return redirect("/")
+
+
+@app.route("/login")
+def show_login_form():
+    """ Show log in form """
+
+    return render_template("login_form.html")
+
+
+@app.route("/login", methods=['POST'])
+def process_login():
+    """ Check if username and password match and if so, log in """
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    user = User.query.filter_by(email=email).first()
+
+    if user.password == password:
+        session['current_user'] = email
+        flash("Logged in as %s" % email)
+        return redirect("/")
+    else:
+        flash("Wrong password!")
+        return redirect("/login")
+
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
