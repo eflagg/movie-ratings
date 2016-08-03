c4c44a761b12cf3b3022c9a087327fa6477abcb3
Hackbright Student <info@hackbrightacademy.com>
2016-08-03 12:36:19 -0700

    added similarity and predict rating functions

--- model.py
+++ model.py
@@ -2,6 +2,8 @@
 
 from flask_sqlalchemy import SQLAlchemy
 
+import correlation
+
 # This is the connection to the PostgreSQL database; we're getting this through
 # the Flask-SQLAlchemy helper library. On this, we can find the `session`
 # object, where we do most of our interactions (like committing, etc.)
@@ -28,6 +30,49 @@ class User(db.Model):
         """Provide helpful representation when printed."""
 
         return "<User user_id=%s email=%s>" % (self.user_id, self.email)
+
+
+    def similarity(self, other):
+        """Return Pearson rating for user compared to other user."""
+
+        u_ratings = {}
+        paired_ratings = []
+
+        for r in self.ratings:
+            u_ratings[r.movie_id] = r
+
+        for r in other.ratings:
+            u_r = u_ratings.get(r.movie_id)
+            if u_r:
+                paired_ratings.append( (u_r.score, r.score) )
+
+        if paired_ratings:
+            return correlation.pearson(paired_ratings)
+
+        else:
+            return 0.0
+
+
+    def predict_rating(self, movie):
+        """ Return prediction for movie based on user with most similarity's 
+            rating for said movie"""
+
+        other_ratings = movie.ratings
+        other_users = [ r.user for r in other_ratings ]
+
+        similarities = [
+            (self.similarity(other_user), other_user)
+            for other_user in other_users
+        ]
+
+        similarities.sort(reverse=True)
+        sim, best_match_user = similarities[0]
+
+        matched_rating = None
+        for rating in other_ratings:
+            if rating.user_id == best_match_user.user_id:
+
+                return rating.score * sim
 
 
 # Put your Movie and Rating model classes here.
